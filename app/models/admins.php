820007ed5c84eb1e81cf8c424658b90a80fd8e0b
Clint.Abz <abz.devs@gmail.com>
2019-12-10 22:55:47 +0800

    Check.

--- app/models/admins.php
+++ app/models/admins.php
@@ -53,15 +53,14 @@ class Admins
 	}
 
 	
-	function latestMsgUser($id)
-	{
-		$this->db->query("SELECT user.firstname AS firstN, user.lastname AS lastN, user_profile.img_path AS imagePath, user.user_availability AS uStatus FROM user LEFT JOIN user_profile ON user_profile.user_id = user.id WHERE user.id = $id");
+	public function getUserMsg($id){
+		$this->db->query("SELECT DISTINCT user.id,user.firstname AS firstN, user.lastname AS lastN, user.user_availability AS uStatus,(SELECT messages.msg_content FROM messages WHERE messages.user_sender_id = user.id ORDER BY messages.timestamp DESC LIMIT 1) AS latestM,(SELECT messages.timestamp FROM messages WHERE messages.user_sender_id = user.id ORDER BY messages.timestamp DESC LIMIT 1) AS mStamp, user_profile.img_path AS imagePath FROM messages LEFT JOIN user ON user.id = messages.user_sender_id LEFT JOIN user_profile ON user_profile.user_id = user.id WHERE user.id != 1 AND (messages.user_receiver_id = 1 AND user.user_type = $id ) ORDER BY mStamp DESC");
 		$row = $this->db->resultSet();
-	   if ($row) {
-		   return $row;
-	   } else {
-		   return false;
-	   }
+		if($row){
+			return $row;
+		}else{
+			return false;
+		}
 	}
 
 	public function getLatestMessages($receiverId, $senderId){
