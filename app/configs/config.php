535951d950744665e4d93a022136c4c4fc526bd5
Clint.Abz <abz.devs@gmail.com>
2019-12-10 16:32:29 +0800

    To remote production. Latest

--- app/configs/config.php
+++ app/configs/config.php
@@ -11,7 +11,7 @@
 			define('APP_ROOT', dirname(dirname(__FILE__)));
 
 			//URL ROOT
-			define('URL_ROOT', 'http://40.88.51.83');
+			define('URL_ROOT', $_SERVER['DOCUMENT_ROOT']);
 
 			//SITE NAME
 			define('SITE_NAME', 'Easy-Ride Terminal Hub');
