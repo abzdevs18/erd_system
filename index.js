3bbe427e6ae6f8fe45fc38d2ed36685bd1845c41
Clint.Abz <abz.devs@gmail.com>
2019-12-12 09:44:52 +0800

    sms

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var socket = require('socket.io');
-const client = require('twilio')(
-	'AC41a05b1c14d7656db36da1b69df879aa',
-	'bc7fa43ea8564869d3b6f1bafdef4958'
-  );
+// const client = require('twilio')(
+// 	'AC41a05b1c14d7656db36da1b69df879aa',
+// 	'bc7fa43ea8564869d3b6f1bafdef4958'
+//   );
 var app = express();
 
 var server = app.listen(3000,function(){
@@ -24,11 +24,11 @@ io.on('connection', function(socket){
 		console.log(data);
 	});
 });
-client.messages.create({
-  from: "+12564725511",
-  to: '+639350976412',
-  body: "You just sent an SMS from Node.js using Twilio!"
-}).then((messsage) => console.log(message.sid));
+// client.messages.create({
+//   from: "+12564725511",
+//   to: '+639350976412',
+//   body: "You just sent an SMS from Node.js using Twilio!"
+// }).then((messsage) => console.log(message.sid));
 
 
 
